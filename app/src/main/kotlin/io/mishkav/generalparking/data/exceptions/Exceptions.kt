7162a34d26e13d4532a76bd89ba609689d726bda
Михаил Ворожцов <MAVorozhtsov@sberbank.ru>
2022-05-02 11:39:12 +0300

    added check min sdk

--- app/src/main/kotlin/io/mishkav/generalparking/data/exceptions/Exceptions.kt
+++ app/src/main/kotlin/io/mishkav/generalparking/data/exceptions/Exceptions.kt
@@ -12,12 +12,14 @@ class NullUserException(
     @StringRes messageRes: Int = R.string.error_auth
 ) : Exception(messageRes.toString())
 
-
 class PlaceReservationException(
     @StringRes messageRes: Int = R.string.error_place_reserved
 ) : Exception(messageRes.toString())
 
-
 class PlaceNotReservatedException(
     @StringRes messageRes: Int = R.string.error_place_reserved
+) : Exception(messageRes.toString())
+
+class MinSdkVersionException(
+    @StringRes messageRes: Int = R.string.error_min_sdk_version
 ) : Exception(messageRes.toString())
--- app/src/main/kotlin/io/mishkav/generalparking/data/repositories/AuthDatabaseRepository.kt
+++ app/src/main/kotlin/io/mishkav/generalparking/data/repositories/AuthDatabaseRepository.kt
@@ -1,5 +1,6 @@
 package io.mishkav.generalparking.data.repositories
 
+import androidx.compose.ui.res.stringResource
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import io.mishkav.generalparking.data.exceptions.NullUserException
@@ -85,10 +86,22 @@ class AuthDatabaseRepository @Inject constructor(
         }
     }
 
+    override suspend fun isMinSdkVersionApproved(): Boolean {
+        val minSdkVersionBack = firebaseDatabase
+            .child(PATH_TO_MIN_SDK)
+            .get()
+            .await()
+
+        return minSdkVersionBack.getValue(CLASS_FLOAT)!! <= MIN_SDK_VERSION
+    }
+
     companion object {
         private const val PATH_TO_USERS = "users"
+        private const val PATH_TO_MIN_SDK = "minimumClientVersion"
+        private const val MIN_SDK_VERSION = 1.0f
 
         private val CLASS_STRING = String::class.java
         private val CLASS_INT = Int::class.java
+        private val CLASS_FLOAT = Float::class.java
     }
 }
